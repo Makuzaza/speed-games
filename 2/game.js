e0c14dfea39a8369c38f386c34a0b8c998dec13a
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-10-02 11:13:08 +0300

    trying to fix game buttons

--- 2/game.js
+++ 2/game.js
@@ -2,12 +2,18 @@ const startButton = document.querySelector('#startButton');
 const endButton = document.querySelector('#endButton');
 const circles = document.querySelectorAll('.circle');
 const scoreDisplay = document.querySelector('.score')
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close');
+const gameOverMessageContainer = document.querySelector('#gameOverMessageContainer');
+const finalScoreDisplay = document.querySelector('#finalScore');
+
 
 let score = 0;
 let timer;
 let pace = 1000;
 let active = 0;
 let rounds = 0;
+let gameRun = false;
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,6 +29,8 @@ const enableEvents = () => {
     })
 }
 
+// count iterations of clicks, if click !== active button 
+
 const clickCircle = (i) => {
     if (i !== active) {
         return endGame();
@@ -32,14 +40,31 @@ const clickCircle = (i) => {
     scoreDisplay.textContent = score;
 }
 
+// count circle
 circles.forEach((circle, i) => {
     circle.addEventListener('click', () => clickCircle(i));
 })
+
+// the function sets the correct button visibility
+const changeButton = () => {
+    if (gameRun) {
+        startButton.style.display = 'none';
+        endButton.style.display = 'block';
+    } else {
+        startButton.style.display = 'block';
+        endButton.style.display = 'none';
+    }
+};
 
 const startGame = () => {
     if (rounds >= 3) {
         return endGame();
     }
+
+    gameRun = true;
+    changeButton();
+
+    enableEvents();
 
     const newActive = pickNew(active);
 
@@ -61,16 +86,42 @@ const startGame = () => {
     console.log(newActive);
 }
 
-const endGame = () => {
-    console.log('game ended');
-    clearTimeout(timer);
-    resetGame();
-}
-
 const resetGame = () => {
     window.location.reload();
 }
 
+changeButton();
+
+
+const updateGameOverMessage = (score) => {
+    finalScoreDisplay.textContent = score;
+    showModal(); // Show the modal with the game over message and final score
+}
+
+const showModal = () => {
+    overlay.classList.add('visible');
+}
+
+const hideModal = () => {
+    overlay.classList.remove('visible');
+};
+
+
+const endGame = () => {
+    console.log('game ended');
+    gameRun = false;
+    changeButton();
+    clearTimeout(timer);
+    updateGameOverMessage(score);
+    showModal();
+    resetGame;
+};
+
 startButton.addEventListener('click', startGame);
 endButton.addEventListener('click', endGame);
+closeButton.addEventListener('click', hideModal);
+
+
+
+
 
